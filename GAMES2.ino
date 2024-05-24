0f30219d3e1cff5c1a2aa67852cc4de5d6251ebf
AdvaOhana <adva1230@gmail.com>
2024-05-24 12:45:06 +0300

    קוד שלב ראשון
    כפתור מדליק פעם אחת אבל לא ניתן ללחוץ במקביל על שני כפתורים

---
+++ GAMES2.ino
@@ -0,0 +1,71 @@
+#define redLed 2
+#define greenLed 4
+
+#define redBtn 11
+#define greenBtn 9
+
+#define ARR_LENGTH 2
+bool LedIsOn[ARR_LENGTH];
+int btnsArr[ARR_LENGTH] = {redBtn,greenBtn};
+int ledsArr[ARR_LENGTH] = {redLed,greenLed};
+
+    void ToggleLed(int ledPinNumber){
+      
+    if(LedIsOn[ledPinNumber]){
+      TurnLedOFF(ledPinNumber);
+    }else{
+      TurnLedOn(ledPinNumber);
+    }
+  
+  }
+  void TurnLedOn(int ledPinNumber){
+    LedIsOn[ledPinNumber] = true;
+    digitalWrite(ledsArr[ledPinNumber],HIGH);
+    
+    
+  }
+  void TurnLedOFF(int ledPinNumber){
+     LedIsOn[ledPinNumber] = false;
+    digitalWrite(ledsArr[ledPinNumber],LOW);
+    
+
+  }
+
+
+
+void setup() {
+  Serial.begin(9600);
+  for(int k=0; k<ARR_LENGTH;k++){
+  pinMode(btnsArr[k], INPUT_PULLUP);
+  pinMode(ledsArr[k], OUTPUT);
+  TurnLedOFF(k);
+  }
+}
+void loop() {
+  for(int i=0;i<ARR_LENGTH;i++){
+    if(digitalRead(btnsArr[i])== LOW){
+    ToggleLed(i);
+    while(digitalRead(btnsArr[i])== LOW){
+         delay(50);
+      }
+    }
+  }
+  
+} 
+
+  // if(digitalRead(redBtn)== LOW){
+  //     num++;
+      // while(digitalRead(redBtn)== LOW){
+      //    delay(50);
+      // }
+  //   digitalWrite(redLed,num%2);
+   
+  // }
+
+  //   if(digitalRead(greenBtn)== LOW){
+  //     num++;
+  //     while(digitalRead(greenBtn)== LOW){
+  //        delay(50);
+  //     }
+  //   digitalWrite(greenLed,num%2);
+  // }
